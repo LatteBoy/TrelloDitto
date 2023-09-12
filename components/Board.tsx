c4f61671f0d7ea59c33b1d8cfedf48e5da161af1
Harrison Shih <comedyinlyfe@gmail.com>
2023-09-12 23:25:43 +0700

    first commit

--- components/Board.tsx
+++ components/Board.tsx
@@ -2,16 +2,17 @@ import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
 function Board() {
   return (
-    <DragDropContext>
-        <Droppable droppableId="board" direction="horizontal" type="column">
-            {(provided) => (
-                <div>
-                    {/* rendering all the columns */}
-                </div>
-            )}
-        </Droppable>
+    <h1>HELLO</h1>
+    // <DragDropContext>
+    //     <Droppable droppableId="board" direction="horizontal" type="column">
+    //         {(provided) => (
+    //             <div>
+    //                 {/* rendering all the columns */}
+    //             </div>
+    //         )}
+    //     </Droppable>
 
-    </DragDropContext>
+    // </DragDropContext>
   )
 }
 
