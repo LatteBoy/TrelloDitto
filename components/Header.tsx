f2fd8244b4b55089e4fd9fbecf3ad050a65730e4
Harrison Shih <comedyinlyfe@gmail.com>
2023-09-14 16:11:33 +0700

    Added backend functionality to populate todo list

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ function Header() {
   return (
     <header>
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
-
+        {/* Gradient Layer for background color */}
         <div
             className="
               absolute
